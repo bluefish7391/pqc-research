9f35a6841a811b42fbbc6b2b613a25773e257671
brian09n <bluefish7391@gmail.com>
2026-06-13 17:58:26 -0400

    feat(tc-test): test linux tc to emulate network impairements

---
+++ sandbox/tc-test/load-test.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    duration: '10s',
+    vus: 10
+}
+
+export default function () {
+    http.get('http://localhost:8080');
+    sleep(1);
+}
