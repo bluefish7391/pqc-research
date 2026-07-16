f07774f6baa16ac037df38cd9a1b02e349a7728c
Brian Nguyen <bluefish7391@gmail.com>
2026-07-16 10:12:27 -0400

    feat(locust): log script pid and ppid for debugging purposes

--- sandbox/main/locust/locustfile.py
+++ sandbox/main/locust/locustfile.py
@@ -49,6 +49,7 @@ class TLSHandshakeUser(User):
             # and suppress output.
             # Python temporarily pauses the execution of this specific Locust user thread and 
             # hands control over to the operating system kernel.
+            log.info(f"PRE-FORK pid={os.getpid()} ppid={os.getppid()} time={time.time()}")
             result = subprocess.run(
                 # Array of command-line arguments for the OpenSSL s_client command.
                 [
@@ -63,6 +64,7 @@ class TLSHandshakeUser(User):
                 capture_output=True, # Capture stdout and stderr for analysis.
                 timeout=10, # Set a timeout for the handshake operation to avoid hanging indefinitely. Measured in seconds.
             )
+            log.info(f"POST-FORK pid={os.getpid()} ppid={os.getppid()} time={time.time()} returncode={result.returncode}")
 
             elapsed_ms = (time.perf_counter_ns() - start_ns) // 1_000_000
             
