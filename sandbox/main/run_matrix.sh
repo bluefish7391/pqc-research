673d96b596b3513a07c774dd0f633501fccd4fa8
Brian Nguyen <bluefish7391@gmail.com>
2026-07-16 23:26:51 -0400

    feat(run_matrix): increase concurrent users to 100 to maximize chance of encountering hang

--- sandbox/main/run_matrix.sh
+++ sandbox/main/run_matrix.sh
@@ -35,7 +35,7 @@ declare -A KEM_GROUPS=(
   # ["pure768"]="MLKEM768"
 )
 
-USER_LEVELS=(50)
+USER_LEVELS=(100)
 RTTS=(0 10 25)         # Round-trip time in milliseconds. This is the artificial latency that will be introduced in the network emulation.
 LOSS_LEVELS=(0 1 2)  # Packet loss percentage. This is the percentage of packets that will be randomly dropped in the network emulation.
 
