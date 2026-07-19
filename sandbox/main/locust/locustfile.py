76debbad925aff53678557e14ab7dbe61649c229
Brian Nguyen <bluefish7391@gmail.com>
2026-07-19 17:05:58 -0400

    feat(locust): ensure runner.quit() is only called once per trial

--- sandbox/main/locust/locustfile.py
+++ sandbox/main/locust/locustfile.py
@@ -50,13 +50,16 @@ def dump_greenlets(signum, frame):
 signal.signal(signal.SIGUSR1, dump_greenlets)
 
 completed_handshakes = 0
+_stop_requested = False
 
 def _check_stop_condition(environment):
     global completed_handshakes
     completed_handshakes += 1
-    # if completed_handshakes >= TARGET_HANDSHAKES:
-    #     log.info(f"Target of {TARGET_HANDSHAKES} handshakes reached ({completed_handshakes}). Stopping runner.")
-    #     environment.runner.quit()
+    if completed_handshakes >= TARGET_HANDSHAKES and not _stop_requested:
+        log.info(f"Target of {TARGET_HANDSHAKES} handshakes reached ({completed_handshakes}). Stopping runner.")
+        global _stop_requested
+        _stop_requested = True
+        environment.runner.quit()
 
 # Define a custom Locust user class that performs TLS handshakes using OpenSSL's s_client.
 class TLSHandshakeUser(User):
