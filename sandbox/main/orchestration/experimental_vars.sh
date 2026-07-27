f5cc8463088b041e21375e36d01830ed522ce7d7
Brian Nguyen <bluefish7391@gmail.com>
2026-07-27 18:38:53 -0400

    feat(variables): add associative array of loss-RTT pairs as network condition levels

--- sandbox/main/orchestration/experimental_vars.sh
+++ sandbox/main/orchestration/experimental_vars.sh
@@ -12,6 +12,13 @@ USER_LEVELS=(1 10 25 100) # Number of concurrent users to simulate in Locust. Th
 RTTS=(200)         # Round-trip time in milliseconds. This is the artificial latency that will be introduced in the network emulation.
 LOSS_LEVELS=(0)  # Packet loss percentage. This is the percentage of packets that will be randomly dropped in the network emulation.
 
+declare -A NETWORK_CONDITIONS=(
+  ["B1"]="rtt=10ms loss=0%"
+  ["B2"]="rtt=50ms loss=1%"
+  ["B3"]="rtt=100ms loss=2%"
+  ["B4"]="rtt=200ms loss=5%"
+)
+
 TARGET_HANDSHAKES=1000 # Total number of handshakes to perform in each trial.
 MAX_DURATION="1000s" # Headless Locust run max duration per combination (seconds).
 REPETITIONS_PER_TEST=1 # Number of times to repeat each combination for averaging or variance analysis.
